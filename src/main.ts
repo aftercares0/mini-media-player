91019aea7662fe9285b60f6e49c87ff90222db04
aftercares0 <dhfjriemdkckdmd@gmail.com>
2026-05-17 13:24:03 +0700

    fix the color after disabling/enabling

--- src/main.ts
+++ src/main.ts
@@ -353,7 +353,8 @@ class MiniMediaPlayer extends LitElement {
       ...(scale && { '--mmp-unit': `${40 * scale}px` }),
       ...(this.foregroundColor &&
         this.player.isActive && {
-        '--mmp-generated-accent-color': this.foregroundColor,
+        '--mmp-overlay-accent-color': `var(--mini-media-player-overlay-accent-color, ${this.foregroundColor})`,
+        '--ha-control-color': `var(--mini-media-player-overlay-accent-color, ${this.foregroundColor})`,
       }),
     });
   }
